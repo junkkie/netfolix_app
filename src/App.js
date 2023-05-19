4efe4af4a2d347c2609e1af1d35cf7d45293ab3e
junkkie <jenkem029@gmail.com>
2023-05-19 14:39:38 +0900

    loadingPage

--- src/App.js
+++ src/App.js
@@ -40,33 +40,31 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <Routes>
-        {isLoggedIn ? 
-        <>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path=':movieId' element={<DetailPage />} /> {/* :param값 */}
-            <Route path='search' element={<SearchPage />} />
-            <Route path='profile' element={<Profile isLoggedIn={isLoggedIn} userObj={userObj} />} />
-          </Route>
-        </>
-          :
-          <Route path='/' element={<Auth />} />
-        }
-      </Routes>
-      {/* <Nav />
-      <Banner />
-      <Row title='NETFLIX ORIGINALS' id='NO' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
-      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending} />
-      <Row title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated} />
-      <Row title='Animation Movie' id='AM' fetchUrl={requests.fetchAnimationMovies} />
-      <Row title='Family Movie' id='FM' fetchUrl={requests.fetchFamilyMovies} />
-      <Row title='Adventure Movie' id='AM' fetchUrl={requests.fetchAdventureMovies} />
-      <Row title='Science Fiction Movie' id='SM' fetchUrl={requests.fetchScienceFictionMovies} />
-      <Row title='Action Movie' id='AM' fetchUrl={requests.fetchAction} />
-      <Footer /> */}
-    </div>
+    <>
+      {init ? (
+        <div className="app">
+          <Routes>
+            {isLoggedIn ? 
+            <>
+              <Route path='/' element={<Layout />}>
+                <Route index element={<MainPage />} />
+                <Route path=':movieId' element={<DetailPage />} /> {/* :param값 */}
+                <Route path='search' element={<SearchPage />} />
+                <Route path='profile' element={<Profile isLoggedIn={isLoggedIn} userObj={userObj} />} />
+              </Route>
+            </>
+              :
+              <Route path='/' element={<Auth />} />
+            }
+          </Routes>
+        </div>
+      ):(
+        <div className='loading'>
+          <span>loading...</span>
+        </div>
+      )}
+    </>
+
   );
 }
 
